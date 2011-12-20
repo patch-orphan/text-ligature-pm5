7ce9895d11aeccb3c573a6a600f553de1aff7569
Nick Patch <patch@cpan.org>
2011-12-19 22:35:12 -0500

    new project

---
+++ lib/Text/Ligature.pm
@@ -0,0 +1,42 @@
+package Text::Ligature;
+
+use 5.006;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Text::Ligature - Replace sequences of characters with typographic ligatures
+
+=head1 VERSION
+
+This document describes Text::Ligature version 0.01.
+
+=head1 SYNOPSIS
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 AUTHOR
+
+Nick Patch <patch@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2011 Nick Patch
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
---
+++ t/00-load.t
@@ -0,0 +1,7 @@
+use strict;
+use warnings;
+use Test::More tests => 1;
+
+BEGIN { use_ok 'Text::Ligature' }
+
+diag "Testing Text::Ligature $Text::Ligature::VERSION, Perl $], $^X";
