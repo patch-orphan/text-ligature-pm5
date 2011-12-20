5e540badda4e60296d27e8fc7ebdcc16f45ba424
Nick Patch <patch@cpan.org>
2011-12-19 23:16:51 -0500

    add pod

--- lib/Text/Ligature.pm
+++ lib/Text/Ligature.pm
@@ -3,6 +3,7 @@ package Text::Ligature;
 use 5.006;
 use strict;
 use warnings;
+use utf8;
 
 our $VERSION = '0.01';
 
@@ -22,11 +23,26 @@ This document describes Text::Ligature version 0.01.
 
 =head1 SYNOPSIS
 
-    ...
+    use Text::Ligature qw( to_ligature );
+
+    to_ligature('offloading floral offices refines effectiveness');
+    # returns: oﬄoading ﬂoral oﬃces reﬁnes eﬀectiveness
+
+    from_ligature('oﬄoading ﬂoral oﬃces reﬁnes eﬀectiveness');
+    # returns: offloading floral offices refines effectiveness';
 
 =head1 DESCRIPTION
 
-...
+Replaces sequences of characters with correspending typographic ligatures.
+
+    Ligature    Non-ligature
+    ﬀ           ff
+    ﬁ           fi
+    ﬂ           fl
+    ﬃ           ffi
+    ﬄ           ffl
+    ﬅ           ft
+    ﬆ           st
 
 =head1 AUTHOR
 
